76f182d0d3897e8db825b2b1bf11a7b25ec08c49
Mark  Ridder <mark@kleurbleur.nl>
2022-05-03 13:05:29 +0200

    improved var checking in the pir player ext

--- pir_player_ext.py
+++ pir_player_ext.py
@@ -20,6 +20,7 @@ player_pir_file = "/home/pi/Desktop/whos_afraid/player_pir.py"
 while True:
     if not player_slow_playing and not player_pir_playing and not pir.value:
         print("start player_slow")
+        player_slow_playing = True
         player_slow = subprocess.Popen(['python3', player_slow_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         player_slow_out, player_slow_err = player_slow.communicate()
         if DEBUG == 1:
@@ -28,10 +29,16 @@ while True:
             print('Error :', player_slow_err)        
             print('player_1', player_slow.pid)
             print('returncode', player_slow.returncode)
-        p1_active = True
+    elif player_slow.returncode == 0:
+        print("killing pir player")
+        player_pir.kill()    
+        print("player_slow done")
+        player_slow_playing = False                
     elif pir.value and not player_pir_playing:
         print("pir active")
         player_pir_playing = True
+        print("killing slow player")
+        player_slow_playing = False
         player_slow.kill()
         print("start player_pir")
         player_pir = subprocess.Popen(['python3', player_pir_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -41,8 +48,9 @@ while True:
             print('Output : ', player_pir_out)
             print('Error :', player_pir_err)
             print('returncode', player_pir.returncode)
-        p1_active = False
-    elif player_pir.returncode == 0 and player_pir_playing and pir.value:
-        print("player_2 done")
-        print("start over")
-        p2_active = False
+    elif player_pir.returncode == 0:
+        print("killing pir player")
+        player_pir.kill()
+        print("player_pir done")
+        player_pir_playing = False       
+
