bd43e8cad6b046d74ed13612d7c773af350f1606
Mark  Ridder <mark@kleurbleur.nl>
2022-05-10 19:57:00 +0200

    raspberry pi version

--- player_final_v2.py
+++ player_final_v2.py
@@ -1,15 +1,17 @@
 import time,socket,datetime,json, threading, random
-from gpiozero import PWMOutputDevice
+from gpiozero import PWMOutputDevice, DigitalInputDevice
 
 
 
 # Set the UPD port here
-UDP_PORT = 7777
+UDP_PORT = 6006
 
 # Set the to be loaded slots. 
 # Has to be full paths or else it won't start on boot! 
-play_pir = "PIR_SLOT.json"
-play_slow = "SLOW_SLOT.json"
+play_pir = "/home/pi/Desktop/whos_afraid/PIR_SLOT.json"
+play_slow = "/home/pi/Desktop/whos_afraid/SLOW_SLOT.json"
+path_settings = "/home/pi/Desktop/whos_afraid/settings.json"
+
 
 # Set the debug level
 # 0 = no debug messages, 1 = PIR sensor, 2 = inverter messages, 3 = UDP, 4 = all
@@ -37,6 +39,13 @@ inv_15 = PWMOutputDevice(5)
 inv_16 = PWMOutputDevice(6)
 inv_17 = PWMOutputDevice(13)
 inv_18 = PWMOutputDevice(19)
+pir = DigitalInputDevice(26)
+
+
+print(f"Listening on port {UDP_PORT} for UDP messages from the recorder software.") 
+print("My IP address was hard coded during the exhibition in Stedelijk Museum Schiedam at 192.168.178.49.")
+print("Commands are 'REC filename.json', 'EDIT', 'SHOW', 'STOP' and 'EXIT'.")
+print("Sending values when in EDIT mode works if inverters are connected and programmed on 0 - 3.3V input.")
 
 
 # Declare variables 
@@ -99,7 +108,26 @@ def player (thread_name, dict, last_entry, slot):
         if stop_thread_pir or stop_thread_slow: 
                 break
         if values:
-            print(f"{datetime.datetime.now().time()}: Thread {thread_name} time: {t_check} values: {values}")                                         
+            if DEBUG == 2 or DEBUG == 4:
+                print(f"{datetime.datetime.now().time()}: {thread_name} time: {t_check} values: {values}")   
+            inv_1.value = values[0]
+            inv_2.value = values[1]
+            inv_3.value = values[2]
+            inv_4.value = values[3]
+            inv_5.value = values[4]
+            inv_6.value = values[5]
+            inv_7.value = values[6]
+            inv_8.value = values[7]
+            inv_9.value = values[8]
+            inv_10.value = values[9]
+            inv_11.value = values[10]
+            inv_12.value = values[11]
+            inv_13.value = values[12]
+            inv_14.value = values[13]
+            inv_15.value = values[14]
+            inv_16.value = values[15]
+            inv_17.value = values[16]
+            inv_18.value = values[17]                                           
         if t1 >= last_entry:
             print(f"{datetime.datetime.now().time()}: Thread {thread_name} done playing {slot}")
             t0 = 0
@@ -122,9 +150,14 @@ def interaction():
     global pir_sensor
     global standard_mode
     global interaction_stop
-    # The loop that checks wether the pir sensor is active or not and starts, or kills, the corresponding threads        
+    # The loop that checks wether the pir sensor is active or not and starts, or kills, the corresponding threads    
+    print("interaction has started")    
+    print("standard_mode", standard_mode)
+    print("pir_sensor_active", pir_sensor_active)
+    print("pir_sensor", pir_sensor)
     while True:
         if interaction_stop:
+            print(f"{datetime.datetime.now().time()}: Interaction thread is stopping")
             break
         if not standard_mode and not pir_sensor_active:
             standard_mode = True
@@ -133,7 +166,7 @@ def interaction():
             t_slow.start() 
         if pir_sensor and not pir_sensor_active:
             pir_sensor_active = True
-            print(f"{datetime.datetime.now().time()}: While loop pir_sensor:", pir_sensor)
+            print(f"{datetime.datetime.now().time()}: interaction thread and not pir_sensor_active pir_sensor:", pir_sensor)
             print(f"{datetime.datetime.now().time()}: Slow player is alive:  {t_slow.is_alive()}")
             if t_slow.is_alive():
                 print(f"{datetime.datetime.now().time()}: Attempting to kill player slow")
@@ -155,12 +188,14 @@ def interaction():
 # the function that will spawn as a thread continiously checking the pir sensor
 def pir_input():
     global pir_sensor
+    global pir
     print(f"{datetime.datetime.now().time()}: Detecting pir sensor")
     while True: 
         pir_sensor = pir.value 
         # time.sleep(40)   # DEBUG for non pi checking
         # pir_sensor = random.getrandbits(1) # DEBUG for non pi checking
-        print(f"{datetime.datetime.now().time()}: pir sensor: {pir_sensor}")
+        if pir_sensor and DEBUG == 1:
+            print(f"{datetime.datetime.now().time()}: pir sensor: {pir_sensor}")
 # start the pir sensor thread 
 try:
     pir_sensor_worker = threading.Thread(target=pir_input)
@@ -170,13 +205,34 @@ except:
     quit()
 
 
-
+def stop_inv():
+    global inv_1, inv_2, inv_3, inv_4, inv_5, inv_6, inv_7, inv_8, inv_9, inv_11, inv_12, inv_13, inv_14, inv_15, inv_16, inv_17, inv_18
+    inv_1.value = 0
+    inv_2.value = 0
+    inv_3.value = 0
+    inv_4.value = 0
+    inv_5.value = 0
+    inv_6.value = 0    
+    inv_7.value = 0
+    inv_8.value = 0
+    inv_9.value = 0
+    inv_10.value = 0
+    inv_11.value = 0
+    inv_12.value = 0  
+    inv_13.value = 0
+    inv_14.value = 0
+    inv_15.value = 0
+    inv_16.value = 0
+    inv_17.value = 0
+    inv_18.value = 0
+    print(f"{datetime.datetime.now().time()}: inverters stopped")
+    return
 
 def network_udp():
     global sock
-    global play_mode
+    global play_mode, play_mode_active, pir_sensor_active
     global path_settings
-    global stop_thread_pir, stop_thread_slow, interaction_stop
+    global stop_thread_pir, stop_thread_slow, interaction_stop, standard_mode
     global inv_1, inv_2, inv_3, inv_4, inv_5, inv_6, inv_7, inv_8, inv_9, inv_11, inv_12, inv_13, inv_14, inv_15, inv_16, inv_17, inv_18
     data = '' # empty var for incoming data
     rec = 0
@@ -188,19 +244,29 @@ def network_udp():
         if data:                                                        # only do something when there's data
             if data.startswith("status"):
                 sock.sendto(bytes(play_mode, "utf-8"), (addr[0], UDP_PORT))
-            if data.startswith("show"):                            # if the first part of the list starts with "rec"
+            if data.startswith("SHOW"):                            # if the first part of the list starts with "rec"
                 file_settings = open(path_settings, 'w')
                 file_settings.write(data)    
                 file_settings.close()
+                standard_mode = False
+                stop_thread_pir = False
+                stop_thread_slow = False                
+                interaction_stop = False
+                play_mode_active = False
+                pir_sensor_active = False                
+                stop_inv() 
                 play_mode = 2
                 print("play_mode udp", play_mode)
-            if data.startswith("edit"):
+            if data.startswith("EDIT"):
                 file_settings = open(path_settings, 'w')
                 file_settings.write(data)    
                 file_settings.close()
-                stop_thread_pir = True
                 stop_thread_slow = True
+                stop_thread_pir = True
                 interaction_stop = True
+                play_mode_active = False
+                pir_sensor_active = False
+                stop_inv() 
                 play_mode = 1 
                 print("play_mode udp", play_mode)
             if play_mode == 1:
@@ -259,51 +325,26 @@ def network_udp():
                             }
                         y.append(x)                                         # append the dict to the list 
                 elif decode_list[0].startswith("ST_RC"):                 # if the list starts with "ST_RC"
-                    inv_1.value = 0
-                    inv_2.value = 0
-                    inv_3.value = 0
-                    inv_4.value = 0
-                    inv_5.value = 0
-                    inv_6.value = 0    
-                    inv_7.value = 0
-                    inv_8.value = 0
-                    inv_9.value = 0
-                    inv_10.value = 0
-                    inv_11.value = 0
-                    inv_12.value = 0  
-                    inv_13.value = 0
-                    inv_14.value = 0
-                    inv_15.value = 0
-                    inv_16.value = 0
-                    inv_17.value = 0
-                    inv_18.value = 0    
                     rec = 0
                     json_dump = json.dumps(y, sort_keys=True, ensure_ascii=False) #transfer the list of dicts into a json format
                     f.write(json_dump)                                      # write it to the file opened in "rec"
                     f.close()                                               # close the file  
                     print("done writing file")          
                     del y                                                   # double check to delete to free up memory 
-                    sock.sendto(bytes("STOPPED RECODING", "utf-8"), (addr[0], UDP_PORT))
+                    stop_inv()   
+                    sock.sendto(bytes("STOPPED RECORDING", "utf-8"), (addr[0], UDP_PORT))
                 elif decode_list[0].startswith("STOP"):                 # if the list starts with "STOP"
-                    print("stopping the inverters")                      
-                    inv_1.value = 0
-                    inv_2.value = 0
-                    inv_3.value = 0
-                    inv_4.value = 0
-                    inv_5.value = 0
-                    inv_6.value = 0    
-                    inv_7.value = 0
-                    inv_8.value = 0
-                    inv_9.value = 0
-                    inv_10.value = 0
-                    inv_11.value = 0
-                    inv_12.value = 0  
-                    inv_13.value = 0
-                    inv_14.value = 0
-                    inv_15.value = 0
-                    inv_16.value = 0
-                    inv_17.value = 0
-                    inv_18.value = 0              
+                    stop_inv()
+                    if rec:
+                        rec = 0
+                        json_dump = json.dumps(y, sort_keys=True, ensure_ascii=False) #transfer the list of dicts into a json format
+                        f.write(json_dump)                                      # write it to the file opened in "rec"
+                        f.close()                                               # close the file  
+                        print("done writing file")          
+                        del y                                                   # double check to delete to free up memory 
+                        stop_inv()   
+                        sock.sendto(bytes("STOPPED RECORDING", "utf-8"), (addr[0], UDP_PORT))
+
                     # print(decode_list[0],decode_list[1])                    # debug purposes
 try:
     network_udp_worker = threading.Thread(target=network_udp)
@@ -315,13 +356,12 @@ except:
 
 
 
-path_settings = "settings.json"
 file_settings = open(path_settings, 'r')
 all_char = file_settings.read(20) # read file
-if all_char.startswith("record"):
+if all_char.startswith("EDIT"):
     print("record mode")
     play_mode = 1
-elif all_char.startswith("play"):
+elif all_char.startswith("SHOW"):
     print("play mode")
     play_mode = 2
 file_settings.close()
@@ -335,8 +375,8 @@ file_settings.close()
 while True:
     if play_mode == 2 and not play_mode_active: 
         play_mode_active = True
-        print("beginning play_mode")
         edit_stop = True
+        print("beginning play_mode")
         try:
             interaction_worker = threading.Thread(target=interaction)
             interaction_worker.start()
